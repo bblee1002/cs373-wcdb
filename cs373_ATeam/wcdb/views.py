fa7e9ace94a54614bf5b10256c55370f4249d1c1
Eddie <saenzeddie5@gmail.com>
2013-07-02 23:29:18 -0500

    index template works for wcdb

--- cs373_ATeam/wcdb/views.py
+++ cs373_ATeam/wcdb/views.py
@@ -1,7 +1,6 @@
-# Create your views here.
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Hello from views.py") #render(request, 'wcdb/index.html')
+	return render(request, 'wcdb/index.html')
 	
