9af3dcaf06ddb6b907c832c15945ede8d914eb13
bblee1002 <bblee1002@utexas.edu>
2013-07-03 19:23:18 -0500

    resolved issue #15

--- import.py
+++ import.py
@@ -1,6 +1,10 @@
+import os
+os.environ["DJANGO_SETTINGS_MODULE"] = "cs373_ATeam.settings"
 import sys
+from django.conf import settings
 from cs373_ATeam.wcdb.models import Crisis
 import xml.etree.ElementTree as ET
+
 
 #Read XML file
 strXML = ""
@@ -13,8 +17,8 @@ crisis_list = []
 #and add to a list of crisis objects created by Django
 for node in root.iter("Crisis"):
 	temp_crisis = Crisis()
-	temp_crisis.crisisID = node.get(ID)
-	temp_crisis.name= node.get(Name)
+	temp_crisis.crisisID = node.get("ID")
+	temp_crisis.name= node.get("Name")
 	crisis_list.append(temp_crisis)
 
 for crisis in crisis_list:
