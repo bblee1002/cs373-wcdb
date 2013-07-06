6f61332df48007a17ef00ce8ff36bad4b0d77681
Eddie <saenzeddie5@gmail.com>
2013-07-05 20:07:37 -0500

    created skeleton for static pages for crises

--- cs373_ATeam/wcdb/views.py
+++ cs373_ATeam/wcdb/views.py
@@ -3,7 +3,14 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def crisisView(request, crisis_id):
-  return HttpResponse('hello from crysis')
+  if crisis_id == '1':
+    return render(request, 'wcdb/CRI_NSAWRT.html')
+  elif crisis_id == '2':
+    return render(request, 'wcdb/CRI_MEXDRG.html')
+  elif crisis_id == '3':
+    return render(request, 'wcdb/CRI_BEEDIE.html')
+  else:
+    return HttpResponse("no such path")
 
 def orgsView(request, orgs_id):
   return HttpResponse('hello from orgs')
