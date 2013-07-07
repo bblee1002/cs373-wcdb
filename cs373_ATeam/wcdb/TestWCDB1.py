1f368249085a34f78d884e374022dab3b98bc689
Eduardo Saenz <eddies@cs.utexas.edu>
2013-07-07 16:30:41 -0500

    added empty TestWCDB1.py for unittests

---
+++ cs373_ATeam/wcdb/TestWCDB1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+"""
+To test the program:
+    % python TestWCDB1.py >& TestWCDB1.out
+    % chmod ugo+x TestWCDB1.py
+    % TestWCDB1.py >& TestWCDB1.out
+"""
+
+# --- imports ---
+import StringIO
+import unittest
+
+class TestWCDB1 (unittest.TestCase):
+    pass
+
+# ----
+# main
+# ----
+
+print "TestWCDB1.py"
+unittest.main()
+print "Done."
