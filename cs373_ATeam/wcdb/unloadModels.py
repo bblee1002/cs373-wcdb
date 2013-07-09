6dcc6bff2dd6266b93cc41762ad60df2e77b08fd
Shaelyn Watson <shaelyn@cs.utexas.edu>
2013-07-09 18:56:47 -0500

    Export smart

--- cs373_ATeam/wcdb/unloadModels.py
+++ cs373_ATeam/wcdb/unloadModels.py
@@ -8,35 +8,67 @@ def embed_wc (sub_string) :
 	#wc_xml.append(sub_string)
 
 
-#Embed model info under crises, orgs, and ppl
-def embed_info (django_models) :
-	xml_string = ""
-	#Unload info from models into XML
-	for d_model in django_models :
-		#***diff syntax needed here to work (logic holds for now)
-		if d_model == "Crisis" :
-			#handle crisis model info
-			x_string = "<Crisis ID=" + d_model.crisis_ID + " Name=" d_model.name + ">"
-			xml_string.append(x_string)
-		if d_model == "Person" :
-			#handle person model info
-			x_string = "<Person ID=" + d_model.person_ID + " Name=" d_model.name + ">"
-			xml_string.append(x_string)
-		if d_model == "Org" :
-			#handle org model info
-			x_string = "<Organization ID=" + d_model.org_ID + " Name=" d_model.name + ">"
-			xml_string.append(x_string)
-
-
-
 
 #case: no import happened to fill these models
 
 #Access models thrgh arg model_dict = {Crises : [], Orgs : [], Ppl : []}
 def receive_import(model_dict) :
-	#steps through major categories
-	for lkey in model_dict.keys() :
-		#Pass root and list of root's models
-		embed_info(model_dict[lkey])
-
-
+	#export crises models
+	for crisis in filled_models["crises"] :
+		#assumes all crises have an id and name
+        crisis_string = "<Crisis ID=\"" + crisis.crisis_ID + "\" Name=\"" + crisis.name + "\">"
+        #if there are people listed
+        if crisis.people != [] :
+        	p_string = "<People>"
+        	for person in crisis.people :
+        		p_string.append("<Person ID=\"" + person.person_ID + "\" />")
+    		p_string.append("</People>")
+		#if there are orgs listed
+		if crisis.organizations != [] :
+        	o_string = "<Organizations>"
+        	for org in crisis.organizations :
+        		o_string.append("<Org ID=\"" + org.org_ID + "\" />")
+    		o_string.append("</Organizations>")
+		if crisis.kind is not None :
+			k = "<Kind>" + crisis.kind + "</Kind>"
+		if crisis.date is not None :
+			d = "<Date>" + crisis.date + "</Date>"
+		if crisis.time is not None :
+			d = "<Time>" + crisis.time + "</Time>"
+		#if there are locations listed
+		if crisis.locations != [] :
+        	l_string = "<Locations>"
+        	for loc in crisis.locations :
+        		l_string.append("<li>" + loc + "</li>")
+    		l_string.append("</Locations>")
+		#if there are impacts listed
+		if crisis.human_impact != [] :
+        	h_string = "<HumanImpact>"
+        	for hi in crisis.human_impact :
+        		h_string.append("<li>" + hi + "</li>")
+    		h_string.append("</HumanImpact>")
+		#if there are human impacts listed
+		if crisis.human_impact != [] :
+        	h_string = "<HumanImpact>"
+        	for hi in crisis.human_impact :
+        		h_string.append("<li>" + hi + "</li>")
+    		h_string.append("</HumanImpact>")
+		#if there are economic impacts listed
+		if crisis.economic_impact != [] :
+        	e_string = "<EconomicImpact>"
+        	for ei in crisis.human_impact :
+        		e_string.append("<li>" + ei + "</li>")
+    		e_string.append("</EconomicImpact>")
+		#if there are resources listed
+		if crisis.resources_needed != [] :
+        	r_string = "<ResourcesNeeded>"
+        	for rn in crisis.resources_needed :
+        		r_string.append("<li>" + rn + "</li>")
+    		e_string.append("</ResourcesNeeded>")
+		#if there are ways to help
+		if crisis.ways_to_help != [] :
+        	w_string = "<WaysToHelp>"
+        	for wth in crisis.ways_to_help :
+        		r_string.append("<li>" + wth + "</li>")
+    		e_string.append("</WaysToHelp>")
+        
