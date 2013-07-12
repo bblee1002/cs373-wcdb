a7d455254af273b5b876fee1295c01838c8781fb
Eduardo Saenz <eddies@cs.utexas.edu>
2013-07-12 18:41:44 -0500

    updated validate.py to use new filenames

--- Validate.py
+++ Validate.py
@@ -3,13 +3,13 @@
 from lxml import etree
 from StringIO import StringIO
 
-xsd = open('WorldCrises.xsd.xml', 'r')
+xsd = open('WCDB1.xsd.xml', 'r')
 xsd = xsd.read()
 xsd = StringIO(xsd)
 xmlschema_doc = etree.parse(xsd)
 xmlschema = etree.XMLSchema(xmlschema_doc)
 
-xml = open('WorldCrises.xml', 'r')
+xml = open('WCDB1.xml', 'r')
 xml = xml.read()
 xml = StringIO(xml)
 xml_doc = etree.parse(xml)
