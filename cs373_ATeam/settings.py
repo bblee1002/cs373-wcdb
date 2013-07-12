1b91b11c6a71abbaa827516765ebab46971677c3
Eddie <saenzeddie5@gmail.com>
2013-07-12 11:52:34 -0500

    rm'd minixsv & genxmlif from installed_apps

--- cs373_ATeam/settings.py
+++ cs373_ATeam/settings.py
@@ -124,8 +124,8 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'wcdb',
-    'minixsv',
-    'genxmlif',
+    # 'minixsv',
+    # 'genxmlif',
 )
 
 # A sample logging configuration. The only tangible logging
