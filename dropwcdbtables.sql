14b4ba0a359a49eb390585241d1776b832377073
Eduardo Saenz <eddies@cs.utexas.edu>
2013-07-20 12:20:25 -0500

    drop tables like they are hot

---
+++ dropwcdbtables.sql
@@ -0,0 +1,16 @@
+-- dropwcdbtables.sql
+DROP TABLE IF EXISTS auth_group;
+DROP TABLE IF EXISTS auth_group_permissions;
+DROP TABLE IF EXISTS auth_message;
+DROP TABLE IF EXISTS auth_permission;
+DROP TABLE IF EXISTS auth_user;
+DROP TABLE IF EXISTS auth_user_groups;
+DROP TABLE IF EXISTS auth_user_user_permissions;
+DROP TABLE IF EXISTS django_content_type;
+DROP TABLE IF EXISTS django_session;
+DROP TABLE IF EXISTS django_site;
+DROP TABLE IF EXISTS wcdb_crisis;
+DROP TABLE IF EXISTS wcdb_li;
+DROP TABLE IF EXISTS wcdb_org;
+DROP TABLE IF EXISTS wcdb_person;
+COMMIT;
