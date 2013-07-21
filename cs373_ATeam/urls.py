4b8d417104a80262b48c8e8a79fe0f1fd564ec5a
Eduardo Saenz <eddies@cs.utexas.edu>
2013-07-21 14:01:19 -0500

    changed urls to cri_[a-z]

--- cs373_ATeam/urls.py
+++ cs373_ATeam/urls.py
@@ -7,17 +7,11 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('',
     url(r'^$', 'wcdb.views.index'),
     url(r'^import/$', 'wcdb.views.importView'),
-    url(r'^crisis/(?P<crisis_id>\d)$', 'wcdb.views.crisisView'),
-    url(r'^orgs/(?P<orgs_id>\d)$', 'wcdb.views.orgsView'),
-    url(r'^people/(?P<people_id>\d)$', 'wcdb.views.peopleView'),
-    url(r'^export/$', 'wcdb.views.exportView'),
-    url(r'^unittests/$', 'wcdb.views.unittestsView'),
-
-    '''
     url(r'^crisis/(?P<crisis_id>CRI_[A-Z]{6})$', 'wcdb.views.crisisView'),
     url(r'^orgs/(?P<orgs_id>ORG_[A-Z]{6})$', 'wcdb.views.orgsView'),
     url(r'^people/(?P<people_id>PER_[A-Z]{6})$', 'wcdb.views.peopleView'),
-    '''
+    url(r'^export/$', 'wcdb.views.exportView'),
+    url(r'^unittests/$', 'wcdb.views.unittestsView'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
