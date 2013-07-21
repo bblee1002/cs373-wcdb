d4dec3203300864640d5e4eb2f8dccf215204268
Paul Carroll <p.v.carroll@gmail.com>
2013-07-21 15:38:23 -0500

    getCrisis(), etc return list of tuples from relations

--- cs373_ATeam/wcdb/getDbModel.py
+++ cs373_ATeam/wcdb/getDbModel.py
@@ -12,29 +12,42 @@ def getCrisis(id):
   Returns dictionary of individual crisis data
   dict of form = 
   """
-  crisis = Crisis.objects.get(crisis_ID = id)
+  try:
+    crisis = Crisis.objects.get(crisis_ID = id)
+  except:
+    return {}
+
   crisis_dict = {}
 
   # #Create keys of dict and give values
   crisis_dict['name'] = crisis.name
 
-  # if person.kind is not None :
-  crisis_dict['kind'] = crisis.kind
+  if crisis.kind is not None :
+    crisis_dict['kind'] = crisis.kind
 
-  # if person.location is not None :
-  crisis_dict['date'] = crisis.date
+  if crisis.date is not None :
+    crisis_dict['date'] = crisis.date
 
-  crisis_dict['time'] = crisis.time
+  if crisis.time is not None:
+    crisis_dict['time'] = crisis.time
 
   temp_person = []
   temp_orgs = []
-  temp_list = Relations.objects.filter(crisis_ID = id)
+  relations = Relations.objects.filter(crisis_ID = id)
 
-  for a in temp_list :
-    if a.person_ID != None:
-      temp_person.append(a.person_ID)
-    if a.org_ID != None:
-      temp_orgs.append(a.org_ID)
+  for a in relations :
+    if a.person_ID is not None:
+      try:
+        name = Person.objects.get(person_ID = a.person_ID).name
+      except:
+        name = ''
+      temp_person.append((a.person_ID, name))
+    if a.org_ID is not None:
+      try:
+        name = Org.objects.get(org_ID = a.org_ID).name
+      except:
+        name = ''
+      temp_orgs.append((a.org_ID, name))
 
   crisis_dict['people'] = temp_person
   crisis_dict['organizations'] = temp_orgs
@@ -44,10 +57,9 @@ def getCrisis(id):
                   'ContactInfo': [], 'Citations': [], 'ExternalLinks': [],
                   'Images': [], 'Videos': [], 'Maps': [], 'Feeds': []}
 
-  temp_list = []
-  temp_list = Li.objects.filter(model_id = id)
+  li_list = Li.objects.filter(model_id = id)
 
-  for a in temp_list :
+  for a in li_list :
     common_dict[a.kind].append(a)
 
   crisis_dict['common'] = common_dict
@@ -65,31 +77,39 @@ def getPerson(id):
   #assumes all people have an id and name
   # assert person.person_ID != None
   # assert person.name != None
+  try:
+    person = Person.objects.get(person_ID = id)
+  except:
+    return {}
 
-  #***needs to be hooked up
-  #when back end is ready
-  person = Person.objects.get(person_ID = id)
-  print person
   person_dict = {}
 
   # #Create keys of dict and give values
   person_dict['name'] = person.name
 
-  # if person.kind is not None :
-  person_dict['kind'] = person.kind
+  if person.kind is not None :
+    person_dict['kind'] = person.kind
 
-  # if person.location is not None :
-  person_dict['location'] = person.location
+  if person.location is not None :
+    person_dict['location'] = person.location
 
   temp_crisis = []
   temp_orgs = []
-  temp_list = Relations.objects.filter(person_ID = id)
+  relations = Relations.objects.filter(person_ID = id)
 
-  for a in temp_list :
+  for a in relations :
     if a.crisis_ID != None:
-      temp_crisis.append(a.crisis_ID)
+      try:
+        name = Crisis.objects.get(crisis_ID = a.crisis_ID).name
+      except:
+        name = ''
+      temp_crisis.append((a.crisis_ID, name))
     if a.org_ID != None:
-      temp_orgs.append(a.org_ID)
+      try:
+        name = Org.objects.get(org_ID = a.org_ID).name
+      except:
+        name = ''
+      temp_orgs.append((a.org_ID, name))
 
   person_dict['crises'] = temp_crisis
   person_dict['organizations'] = temp_orgs
@@ -99,10 +119,9 @@ def getPerson(id):
                   'ContactInfo': [], 'Citations': [], 'ExternalLinks': [],
                   'Images': [], 'Videos': [], 'Maps': [], 'Feeds': []}
 
-  temp_list = []
-  temp_list = Li.objects.filter(model_id = id)
+  li_list = Li.objects.filter(model_id = id)
 
-  for a in temp_list :
+  for a in li_list :
     common_dict[a.kind].append(a)
 
   person_dict['common'] = common_dict
@@ -116,28 +135,39 @@ def getOrg(id):
   Returns dictionary of individual organization data
   dict of form = 
   """
-  org = Org.objects.get(org_ID = id)
-  print org
+  try:
+    org = Org.objects.get(org_ID = id)
+  except:
+    return {}
+
   org_dict = {}
 
   # #Create keys of dict and give values
   org_dict['name'] = org.name
 
-  # if person.kind is not None :
-  org_dict['kind'] = org.kind
+  if org.kind is not None :
+    org_dict['kind'] = org.kind
 
-  # if person.location is not None :
-  org_dict['location'] = org.location
+  if org.location is not None :
+    org_dict['location'] = org.location
 
   temp_people = []
   temp_crisis = []
-  temp_list = Relations.objects.filter(org_ID = id)
+  relations = Relations.objects.filter(org_ID = id)
 
-  for a in temp_list :
+  for a in relations :
     if a.crisis_ID != None:
-      temp_crisis.append(a.crisis_ID)
+      try:
+        name = Crisis.objects.get(crisis_ID = a.crisis_ID).name
+      except:
+        name = ''
+      temp_crisis.append((a.crisis_ID, name))
     if a.person_ID != None:
-      temp_people.append(a.person_ID)
+      try:
+        name = Person.objects.get(person_ID = a.person_ID).name
+      except:
+        name = ''
+      temp_people.append((a.person_ID, name))
 
   org_dict['crises'] = temp_crisis
   org_dict['people'] = temp_people
@@ -147,10 +177,9 @@ def getOrg(id):
                   'ContactInfo': [], 'Citations': [], 'ExternalLinks': [],
                   'Images': [], 'Videos': [], 'Maps': [], 'Feeds': []}
 
-  temp_list = []
-  temp_list = Li.objects.filter(model_id = id)
+  li_list = Li.objects.filter(model_id = id)
 
-  for a in temp_list :
+  for a in li_list :
     common_dict[a.kind].append(a)
 
   org_dict['common'] = common_dict
