007893c64692cf394715f13d0c129677e36fb162
bblee1002 <bblee1002@utexas.edu>
2013-07-31 13:11:25 -0500

    Modified search to handle exact case

--- cs373_ATeam/wcdb/search.py
+++ cs373_ATeam/wcdb/search.py
@@ -3,24 +3,22 @@ from getDbModel import *
 from django.db.models import Q
 
 def search(query):
-	query = query.upper()
-	searchTerms = query.split()
+	query       =    query.upper()
+	searchTerms =    query.split()
+	numTerms    = len(searchTerms)
 
-	numTerms = len(searchTerms)
+	#exact matching case
+	exactCrises = searchCrisis([query], orCrises)
+	exactPeople = searchPerson([query], orPeople)
+	exactOrgs   =      searchOrg([query], orOrgs)
 
-	orCrises = set()
-	orPeople = set()
-	orOrgs = set()
+	#or case
+	orCrises = searchCrisis(searchTerms, orCrises).difference(exactCrises)
+	orPeople = searchPerson(searchTerms, orPeople).difference(exactPeople)
+	orOrgs   =        searchOrg(searchTerms, orOrgs).difference(exactOrgs)
 
 	matchingCount = {}
-
-
-	# or case
-	for term in searchTerms:
-		orCrises = orCrises.union(Crisis.objects.filter(Q(crisis_ID__iregex = term) | Q(name__iregex = term) | Q(kind__iregex = term) | Q(date__iregex = term) | Q(time__iregex = term) | Q(common_summary__iregex = term)))
-		orPeople = orPeople.union(Person.objects.filter(Q(person_ID__iregex = term) | Q(name__iregex = term) | Q(kind__iregex = term) | Q(location__iregex = term) | Q(common_summary__iregex = term)))
-		orOrgs = orOrgs.union(Org.objects.filter(Q(org_ID__iregex = term) | Q(name__iregex = term) | Q(kind__iregex = term) | Q(location__iregex = term) | Q(common_summary__iregex = term)))
-
+	
 
 	for crisis in orCrises:
 		matchingCount[crisis.crisis_ID] = 0
@@ -41,14 +39,12 @@ def search(query):
 				matchingCount[crisis.crisis_ID] += 1
 
 	for person in orPeople:
-		termCount = 0
 		personString = str(getPerson(person.person_ID)).upper()
 		for term in searchTerms:
 			if term in personString:
 				matchingCount[person.person_ID] += 1
 
 	for org in orOrgs:
-		termCount = 0
 		orgString = str(getOrg(org.org_ID)).upper()
 		for term in searchTerms:
 			if term in orgString:
@@ -76,7 +72,21 @@ def countIncrement(container, orObjects):
 		if not found:
 			matches.add(i)
 
-
+def searchCrisis(searchTerms) :
+	modelSet = {}
+	for term in searchTerms :
+		modelSet = modelSet.union(Crisis.objects.filter(Q(crisis_ID__iregex = term) | Q(name__iregex = term) | Q(kind__iregex = term) | Q(date__iregex = term) | Q(time__iregex = term) | Q(common_summary__iregex = term)))	
+	return modelSet
+def searchPerson(searchTerms) :
+	modelSet = {}
+	for term in searchTerms :
+		modelSet = modelSet.union(Person.objects.filter(Q(person_ID__iregex = term) | Q(name__iregex = term) | Q(kind__iregex = term) | Q(location__iregex = term) | Q(common_summary__iregex = term)))
+	return modelSet			
+def searchOrg(searchTerms) :
+	modelSet = {}
+	for term in searchTerms :
+		modelSet = modelSet.union(Org.objects.filter(Q(org_ID__iregex = term) | Q(name__iregex = term) | Q(kind__iregex = term) | Q(location__iregex = term) | Q(common_summary__iregex = term)))
+	return modelSet		
 
 class match():
 	def __init__(self, id):
