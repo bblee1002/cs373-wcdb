5335f6439e79833f5c0393544685937f42680e18
bblee1002 <bblee1002@utexas.edu>
2013-08-04 15:39:04 -0500

    Resolved issue #95

--- cs373_ATeam/wcdb/search.py
+++ cs373_ATeam/wcdb/search.py
@@ -83,6 +83,7 @@ def search(query) :
 	# 	print res.id
 	getContext(result, matchFound, searchTerms, numTerms)
 	for match in result:
+		print match.idref
 		for context in match.contexts:
 			print "begin: ", context.begin 
 			print "bold: ", context.bold 
@@ -160,101 +161,73 @@ def populateMatchFound(searchTerms, numTerms, matchFound, orCrises, orPeople, or
 			count += 1
 
 def getContext(result, matchFound, searchTerms, numTerms):
+	#going through match instances in result
 	for match in result :
 		# iterate through model:
+		modelDict = {}
+		keyList = []
 		#crisis_dict = {name : *, kind : *, date : *, time : *, people : [], organizations : [], Common : ?}
 		if match.idref[0:3] == "CRI" :
 			modelDict = getCrisis(match.idref)
-			
+			keyList = ['name', 'kind', 'date', 'time','common']
 
-			for index in xrange(numTerms) :
-				if matchFound[match.idref][index] == False :
-					continue
+		#person_dict = {name : *, kind : *, location : *, crises : [], organizations : [], Common : ?}
+		if match.idref[0:3] == "PER" :
+			modelDict = getPerson(match.idref)
+			keyList = ['name', 'kind', 'location', 'common']
 
-				found = modelDict['name'].upper().find(searchTerms[index])
-				if found >= 0 :
-					tempContext = Context()
-					tempContext.begin =  'NAME...'
-					if found > 0 :
-						regex = re.search("[^ ]* *[^ ]* *[^ ]* *[^ ]* *[^ ]*", modelDict['name'][found-1::-1]).group(0)
-						print "SUBSTRING: ", modelDict['name'][found-1::-1]
-						print "BEFORE CONCATENATION: ", regex
-						tempContext.begin += regex[::-1]
-						print "TESTING: ", tempContext.begin
-					tempContext.bold  =  modelDict['name'][found:(found + len(searchTerms[index]))]
-					tempContext.end   += re.search("[^ ]* *[^ ]* *[^ ]* *[^ ]* *[^ ]*", modelDict['name'][found + len(searchTerms[index]): found + 100]).group(0)
-					match.contexts.append(tempContext)
-					continue
+		#org_dict = {name : *, kind : *, location : *, crises : [], organizations : [], Common : ?}
+		if match.idref[0:3] == "PER" :
+			modelDict = getPerson(match.idref)
+			keyList = ['name', 'kind', 'location', 'common']
 
-				found = modelDict['kind'].upper().find(searchTerms[index])
-				if found >= 0 :
-					tempContext = Context()
-					tempContext.begin =  'KIND...'
-					if found > 0 :
-						regex = re.search("[^ ]* *[^ ]* *[^ ]* *[^ ]* *[^ ]*", modelDict['kind'][found-1::-1]).group(0)
-						tempContext.begin += regex[::-1]
-					tempContext.bold  =  modelDict['kind'][found:(found + len(searchTerms[index]))]
-					tempContext.end   += re.search("[^ ]* *[^ ]* *[^ ]* *[^ ]* *[^ ]*", modelDict['kind'][found + len(searchTerms[index]): found + 100]).group(0)
-					match.contexts.append(tempContext)
-					continue
-
-				found = modelDict['date'].upper().find(searchTerms[index])
-				if found >= 0 :
-					tempContext = Context()
-					tempContext.begin =  'DATE...'
-					if found > 0 :
-						regex = re.search("[^ ]* *[^ ]* *[^ ]* *[^ ]* *[^ ]*", modelDict['date'][found-1::-1]).group(0)
-						tempContext.begin += regex[::-1]
-					tempContext.bold  =  modelDict['date'][found:(found + len(searchTerms[index]))]
-					tempContext.end   += re.search("[^ ]* *[^ ]* *[^ ]* *[^ ]* *[^ ]*", modelDict['date'][found + len(searchTerms[index]): found + 100]).group(0)
-					match.contexts.append(tempContext)
-					continu
-
-				found = modelDict['time'].upper().find(searchTerms[index])
-				if found >= 0 :
-					tempContext = Context()
-					tempContext.begin =  'TIME...'
-					if found > 0 :
-						regex = re.search("[^ ]* *[^ ]* *[^ ]* *[^ ]* *[^ ]*", modelDict['time'][found-1::-1]).group(0)
-						tempContext.begin += regex[::-1]
-					tempContext.bold  =  modelDict['time'][found:(found + len(searchTerms[index]))]
-					tempContext.end   += re.search("[^ ]* *[^ ]* *[^ ]* *[^ ]* *[^ ]*", modelDict['time'][found + len(searchTerms[index]): found + 100]).group(0)
-					match.contexts.append(tempContext)
-					continue
-
-				found = modelDict['common']['Summary'].upper().find(searchTerms[index])
-				if found >= 0 :
-					tempContext = Context()
-					tempContext.begin =  'DATE...'
-					if found > 0 :
-						regex = re.search("[^ ]* *[^ ]* *[^ ]* *[^ ]* *[^ ]*", modelDict['common']['Summary'][found-1::-1]).group(0)
-						tempContext.begin += regex[::-1]
-					tempContext.bold  =  modelDict['common']['Summary'][found:(found + len(searchTerms[index]))]
-					tempContext.end   += re.search("[^ ]* *[^ ]* *[^ ]* *[^ ]* *[^ ]*", modelDict['common']['Summary'][found + len(searchTerms[index]): found + 100]).group(0)
-					match.contexts.append(tempContext)
-					continue
-
-		liDict = getLi(match.idref)
 		for index in xrange(numTerms) :
+			#checks to see if this page has a match for this term
 			if matchFound[match.idref][index] == False :
 				continue
-
-			found = liDict['floating_text'][index].upper().find(searchTerms[index])
-			if found >= 0 :
-				tempContext = Context()
-				tempContext.begin = liDict['kind'][index] + '...'
-				if found > 0 :
-					regex = re.search("[^ ]* *[^ ]* *[^ ]* *[^ ]* *[^ ]*", liDict['floating_text'][index][found-1::-1]).group(0)
-					#print "SUBSTRING: ", liDict['floating_text'][index][found-1::-1]
-					#print "BEFORE CONCATENATION: ", regex
-					tempContext.begin += regex[::-1]
-					#print "TESTING: ", tempContext.begin
-				tempContext.bold  =  liDict['floating_text'][index][found:(found + len(searchTerms[index]))]
-				tempContext.end   += re.search("[^ ]* *[^ ]* *[^ ]* *[^ ]* *[^ ]*", liDict['floating_text'][index][found + len(searchTerms[index]): found + 100]).group(0)
-				match.contexts.append(tempContext)
-				continue
+			for key in keyList :
+				if key != 'common' :
+					found = modelDict[key].upper().find(searchTerms[index])
+					if found >= 0 :
+						getContextFromModel(match, modelDict, searchTerms, index, key)
+						break
+				else :
+					#common case is different, since it's a nested container
+					found = modelDict['common']['Summary'].upper().find(searchTerms[index])
+					#break
+					if found >= 0 :
+						getContextFromModel(match, modelDict['common'], searchTerms, index, 'Summary')
+						break
+			#none of the attributes found it so it must be in the Li attributes
+			else : 
+				liDict = getLi(match.idref)
+				# print len(liDict['floating_text'])
+				# print len(liDict['kind'])
+				for liIndex in xrange(len(liDict['kind'])) :
+					found = liDict['floating_text'][liIndex].upper().find(searchTerms[index])
+					if found >= 0 :
+						tempContext = Context()
+						tempContext.begin = liDict['kind'][liIndex] + '...'
+						if found > 0 :
+							regex = re.search("[^ ]* *[^ ]* *[^ ]* *[^ ]* *[^ ]*", liDict['floating_text'][liIndex][found-1::-1]).group(0)
+							tempContext.begin += regex[::-1]
+						tempContext.bold  =  liDict['floating_text'][liIndex][found:(found + len(searchTerms[index]))]
+						tempContext.end   += re.search("[^ ]* *[^ ]* *[^ ]* *[^ ]* *[^ ]*", liDict['floating_text'][liIndex][found + len(searchTerms[index]): found + 100]).group(0)
+						match.contexts.append(tempContext)
+						break
+		# 		continue
 
 		# iterate through Li:
+def getContextFromModel(match, modelDict, searchTerms, index, attribute) :
+	found = modelDict[attribute].upper().find(searchTerms[index])
+	tempContext = Context()
+	tempContext.begin =  attribute.upper() + '...'
+	if found > 0 :
+		regex = re.search("[^ ]* *[^ ]* *[^ ]* *[^ ]* *[^ ]*", modelDict[attribute][found-1::-1]).group(0)
+		tempContext.begin += regex[::-1]
+	tempContext.bold  =  modelDict[attribute][found:(found + len(searchTerms[index]))]
+	tempContext.end   += re.search("[^ ]* *[^ ]* *[^ ]* *[^ ]* *[^ ]*", modelDict[attribute][found + len(searchTerms[index]): found + 100]).group(0)
+	match.contexts.append(tempContext)
 
 class Match() :
 	def __init__(self, idref, count) :
